2feabc70ec94677d338047771acc2127c4526d48
vorkuta <vorkutavorkutlag@gmail.com>
2026-01-29 08:39:04 +0100

    main: began work on bencode skeleton

--- bencode.cpp
+++ bencode.cpp
@@ -1,19 +1,67 @@
-#include <map>
-
 #include <variant>
 #include <map>
 #include <vector>
 #include <string>
 #include <cstdint>
 
-using BencodeValue = std::variant<
-    std::int64_t,                    
-    std::string,                     
-    std::vector<BencodeValue>,       
-    std::map<std::string, BencodeValue> 
->;
+#include <fstream>
+
+struct BencodeValue;
+
+using BencodeList = std::vector<BencodeValue>;
+using BencodeDict = std::map<std::string, BencodeValue>;
+
+struct BencodeValue : std::variant<
+    std::int64_t,
+    std::string,
+    BencodeList,
+    BencodeDict
+> {
+    using variant::variant;
+};
+
+enum class BencodeType : char {
+  INTEGER = 'i',
+  LIST = 'l',
+  DICT = 'd',
+  END = 'e',
+};
+
+BencodeValue decode(std::ifstream& file) {
+  char c;
+  file.get(c);
+  
+  switch(c) {
+    
+    case static_cast<char>(BencodeType::INTEGER): {
+      std::int64_t int_val = 0;
+      
+      while (file.get(c)) {
+        if (c == static_cast<char>(BencodeType::END)) break;
+        int_val *= 10;
+        int_val += c - '0';
+      }
+      
+      return BencodeValue{int_val};
+    }
+    
+    case static_cast<char>(BencodeType::LIST):
+      // Would parse list here
+      return BencodeValue{BencodeList{}};
+    
+    case static_cast<char>(BencodeType::DICT):
+      // Would parse dict here
+      return BencodeValue{BencodeDict{}};
+    
+    default: // BYTE_STRING
+      file.unget(); // Put digit back
+      // Would parse string here
+      return BencodeValue{std::string{}};
+      
+  }
+}
 
 int main() {
-  
+
   return 0;
 }
